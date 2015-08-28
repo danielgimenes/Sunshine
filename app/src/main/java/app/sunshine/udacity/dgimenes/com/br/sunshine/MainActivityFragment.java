b046040195134f30377473b2fca004d0dc095f75
Daniel Gimenes <danielcgimenes@gmail.com>
2015-08-28 14:47:52 +0000

    listview with fake data

--- app/src/main/java/app/sunshine/udacity/dgimenes/com/br/sunshine/MainActivityFragment.java
+++ app/src/main/java/app/sunshine/udacity/dgimenes/com/br/sunshine/MainActivityFragment.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -18,6 +24,20 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        String[] fakeForecastData = {
+                "Today - Sunny - 22 / 16",
+                "Tomorrow - Sunny - 21 / 17",
+                "Saturday - Cloudy - 20 / 15",
+                "Sunday - Sunny - 25 / 19",
+                "Monday - Rainy - 20 / 12"
+        };
+        List<String> weekForecast = new ArrayList<>();
+        weekForecast.addAll(Arrays.asList(fakeForecastData));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                R.layout.list_item_forecast, R.id.list_view_forecast_textview, weekForecast);
+        ListView forecastListView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        forecastListView.setAdapter(adapter);
+        return rootView;
     }
 }
