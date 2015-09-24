e352d775c260ba947ec96cf29f24df0c2b6d3733
Daniel Gimenes <danielcgimenes@gmail.com>
2015-09-23 22:40:46 -0300

    WeatherContract.buildWeatherLocation

--- app/src/main/java/br/com/dgimenes/sunshine/data/WeatherContract.java
+++ app/src/main/java/br/com/dgimenes/sunshine/data/WeatherContract.java
@@ -132,8 +132,8 @@ public class WeatherContract {
         /*
             Student: Fill in this buildWeatherLocation function
          */
-        public static Uri buildWeatherLocation(String locationSetting) {
-            return null;
+            public static Uri buildWeatherLocation(String locationSetting) {
+            return CONTENT_URI.buildUpon().appendPath(locationSetting).build();
         }
 
         public static Uri buildWeatherLocationWithStartDate(
